a4b5abbef74ad1c03daf4fb39e35101f0d56f9e9
Pavel Máca <maca.pavel@gmail.com>
2016-04-26 23:14:23 +0200

    shape generator using create method from enum Type

--- src/tetris/engine/ShapeGenerator.java
+++ src/tetris/engine/ShapeGenerator.java
@@ -52,21 +52,7 @@ public class ShapeGenerator {
     }
 
     public Shape createNext() {
-        Shape newShape = null;
-
-        switch (getRandomType()) {
-            case LINE:
-                newShape = createLine();
-                break;
-            case CORNER:
-                newShape = createCorner();
-                break;
-            case T:
-                newShape = createT();
-                break;
-            default:
-                //throw new Exception("Requested shape cant be generated.");
-        }
+        Shape newShape = getRandomType().create();
 
         randomFlip(newShape);
         randomRotation(newShape);
@@ -74,34 +60,69 @@ public class ShapeGenerator {
         return newShape;
     }
 
-    public Shape createLine() {
-        Shape line = new Shape(3,3, Color.black);
-        line.addPoint(1, 0);
-        line.addPoint(1, 1);
-        line.addPoint(1, 2);
-
-        return line;
-    }
-
-    public Shape createCorner() {
-        Shape corner = new Shape(2, 2, Color.black);
-        corner.addPoint(0, 0);
-        corner.addPoint(0, 1);
-        corner.addPoint(1, 0);
-        return corner;
-    }
-
-    public Shape createT() {
-        Shape t = new Shape(3, 3, Color.black);
-        t.addPoint(0, 0);
-        t.addPoint(0, 1);
-        t.addPoint(0, 2);
-        t.addPoint(1, 1);
-        return t;
-    }
 
     public enum Type {
-        LINE, CORNER, T//, C;
+        LINE {
+            public Shape create() {
+                Shape line = new Shape(3,3, Color.black);
+                line.addPoint(1, 0);
+                line.addPoint(1, 1);
+                line.addPoint(1, 2);
+
+                return line;
+            }
+        },
+        CORNER {
+            public Shape create() {
+                Shape corner = new Shape(2, 2, Color.black);
+                corner.addPoint(0, 0);
+                corner.addPoint(0, 1);
+                corner.addPoint(1, 0);
+                return corner;
+            }
+        },
+        T{
+            public Shape create() {
+                Shape t = new Shape(3, 3, Color.black);
+                t.addPoint(0, 0);
+                t.addPoint(0, 1);
+                t.addPoint(0, 2);
+                t.addPoint(1, 1);
+                return t;
+            }
+        },
+        SQUARE {
+            public Shape create() {
+                Shape square = new Shape(2, 2, Color.black);
+                square.addPoint(0, 0);
+                square.addPoint(0, 1);
+                square.addPoint(1, 0);
+                square.addPoint(1, 1);
+                return square;
+            }
+        },
+        N {
+            public Shape create() {
+                Shape n = new Shape(3, 3, Color.black);
+                n.addPoint(0, 0);
+                n.addPoint(0, 1);
+                n.addPoint(1, 1);
+                n.addPoint(1, 2);
+                return n;
+            }
+        },
+        L {
+            public Shape create() {
+                Shape l = new Shape(3, 3, Color.black);
+                l.addPoint(0, 0);
+                l.addPoint(0, 1);
+                l.addPoint(0, 2);
+                l.addPoint(1, 2);
+                return l;
+            }
+        };
+
+        public abstract Shape create();
     }
 
 }
